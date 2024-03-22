621ea5755e65c5c4ee643277def0fa400373434b
critbase <12738442+critbase@users.noreply.github.com>
2024-03-22 17:28:16 -0400

    update config to new format

--- configs/cutebox/BoardConfig.h
+++ configs/cutebox/BoardConfig.h
@@ -1,49 +1,34 @@
 #ifndef PICO_BOARD_CONFIG_H_
 #define PICO_BOARD_CONFIG_H_
 
-#include <GamepadEnums.h>
+#include "enums.pb.h"
 
-#define PIN_DPAD_UP 9
-#define PIN_DPAD_DOWN 28
-#define PIN_DPAD_RIGHT 27
-#define PIN_DPAD_LEFT 29
+#define BOARD_CONFIG_LABEL "cutebox"
 
-#define PIN_BUTTON_B1 25
-#define PIN_BUTTON_B2 23
-#define PIN_BUTTON_B3 26
-#define PIN_BUTTON_B4 24
+#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_UP
+#define GPIO_PIN_28 GpioAction::BUTTON_PRESS_DOWN
+#define GPIO_PIN_27 GpioAction::BUTTON_PRESS_RIGHT
+#define GPIO_PIN_29 GpioAction::BUTTON_PRESS_LEFT
 
-#define PIN_BUTTON_R1 13
-#define PIN_BUTTON_R2 12
-#define PIN_BUTTON_L1 12
-#define PIN_BUTTON_L2 10
+#define GPIO_PIN_25 GpioAction::BUTTON_PRESS_B1
+#define GPIO_PIN_23 GpioAction::BUTTON_PRESS_B2
+#define GPIO_PIN_26 GpioAction::BUTTON_PRESS_B3
+#define GPIO_PIN_24 GpioAction::BUTTON_PRESS_B4
 
-#define PIN_BUTTON_S1 0
-#define PIN_BUTTON_S2 1
-#define PIN_BUTTON_A1 4
-#define PIN_BUTTON_A2 5
-#define PIN_BUTTON_R3 3
-#define PIN_BUTTON_L3 2
+#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_R1
+#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_R2
+#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_L1
+#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_L2
 
-#define PIN_BUTTON_TURBO -1
-#define PIN_SLIDER_LS -1
-#define PIN_SLIDER_RS -1
+#define GPIO_PIN_00 GpioAction::BUTTON_PRESS_S1
+#define GPIO_PIN_01 GpioAction::BUTTON_PRESS_S2
+#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_A1
+#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_A2
+#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_R3
+#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_L3
 
 #define DEFAULT_SOCD_MODE SOCD_MODE_UP_PRIORITY
 #define BUTTON_LAYOUT BUTTON_LAYOUT_STICKLESS
 #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_NOIR8
-
-#define SPLASH_MODE STATICSPLASH
-#define TURBO_LED_PIN -1
-#define BOARD_LEDS_PIN -1
-
-#define HAS_I2C_DISPLAY 1
-#define I2C_SDA_PIN 8
-#define I2C_SCL_PIN 9
-#define I2C_BLOCK i2c0
-#define I2C_SPEED 400000
-
-#define ANALOG_ADC_VRY -1
-#define ANALOG_ADC_VRX -1
 
 #endif
